c0f71f4595cf7e33c80df923c59afd183425aeab
nischal <nischal.bhatt.87@gmail.com>
2022-03-10 13:18:46 +0800

    passing file name as job param during run configuration

--- src/main/java/com/infybuzz/config/SampleJob.java
+++ src/main/java/com/infybuzz/config/SampleJob.java
@@ -6,12 +6,14 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -50,19 +52,20 @@ public class SampleJob {
 	private Step firstChunkStep() {
 		return stepBuilderFactory.get("First Chunk Step")
 				.<StudentCsv, StudentCsv>chunk(3)
-				.reader(flatFileItemReader())
+				.reader(flatFileItemReader(null))
 				//.processor(firstItemProcessor)
 				.writer(firstItemWriter)
 				.build();
 	}
 
-	public FlatFileItemReader<StudentCsv> flatFileItemReader()
+	@StepScope
+	@Bean
+	public FlatFileItemReader<StudentCsv> flatFileItemReader(@Value ("#{jobParameters['inputFile']}") FileSystemResource fileSystemResource)
 	{
 		FlatFileItemReader<StudentCsv> flatFileItemReader = 
 				new FlatFileItemReader<StudentCsv>();
 		
-		flatFileItemReader.setResource(new FileSystemResource(
-				new File("C:\\Users\\devbh\\Downloads\\New folder (12)\\all-about-readers\\inputfiles\\students.csv")));
+		flatFileItemReader.setResource(fileSystemResource);
 		
 		flatFileItemReader.setLineMapper(new DefaultLineMapper<StudentCsv>() {
 			{
